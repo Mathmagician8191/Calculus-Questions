c11cc1a76aa961529f30acd5e93c546dbf815c85
Mathmagician8191 <50558333+Mathmagician8191@users.noreply.github.com>
2019-06-19 12:37:47 +1200

    Beginning JS development
    Added semi-colons and began generate function

--- javascript/function_generate.js
+++ javascript/function_generate.js
@@ -1,8 +1,9 @@
-var math = require("algebrite")
+var math = require("algebrite");
+var normal = require("random-normal");
 
 function interior(terms, cascade, level3) {
   if (Math.random() < cascade && terms > 1) {
-    return "(" + generate(terms - 1, cascade, level3) + ")"
+    return "(" + generate(terms - 1, cascade, level3) + ")";
   }
   else {return "(x)"}
 }
@@ -13,5 +14,11 @@ function generate(terms=4, cascade=0.3, level3=False) {
     * Cascade is the chance to use a function as an argument
     * Level 3 controls whether Level 3 functions are used.
     */
-  return 0
+  var function = "";
+  while (Math.random() > (1/terms) || function == "") {
+    //adds a coefficient
+    var coefficient = Math.floor(normal({mean: 1, dev: 3}));
+    function += coefficient.toString() + "*";
+  }
+  return function;
 }
