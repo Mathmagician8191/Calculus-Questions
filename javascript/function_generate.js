41fdd4f9d774f31ab6dda7b5e8476e281821feef
Mathmagician8191 <50558333+Mathmagician8191@users.noreply.github.com>
2019-08-13 11:26:50 +1200

    Changed colours
    Made level 2 green, l3 easy yellow, l3 medium orange and l3 hard red
    All mixed with white to make it lighter

--- javascript/function_generate.js
+++ javascript/function_generate.js
@@ -72,25 +72,25 @@ function gen(difficulty) {
 			terms = 3;
 			cascade = 0.05;
 			level3 = false;
-			colour.background = "#99ffcc";
+			colour.background = "#99ff99";
 			break;
 		case "easy":
 			terms = 3;
 			cascade = 0.1;
 			level3 = true;
-			colour.background = "#99ff99";
+			colour.background = "#ffff99";
 			break;
 		case "medium":
 			terms = 4;
 			cascade = 0.25;
 			level3 = false;
-			colour.background = "#ffff00";
+			colour.background = "#ffcc99";
 			break;
 		case "hard":
 			terms = 5;
 			cascade = 0.4;
 			level3 = true;
-			colour.background = "#ff9900";
+			colour.background = "#ff9999";
 			break;
 	}
 	document.getElementById("latex").innerHTML = "Loading...";
